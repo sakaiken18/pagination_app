cb3331fc183e9a38e8cce1f84fdbf140cf3b4b83
sakaiken <sakaiken@sakaikennoMacBook-Pro.local>
2021-12-08 22:18:35 +0900

    投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,13 @@ class PostsController < ApplicationController
   PER_PAGE = 10
 
   def index
+    @post = Post.new
     @posts = Post.page(params[:page]).per(PER_PAGE).order("id DESC")
   end
 
   def create
     post = Post.create!(post_params)
+    redirect_to root_path
   end
 end
 
